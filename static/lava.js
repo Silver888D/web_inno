9d1602006ee7128dc32f2091d518cf323434e823
Seb Amos <sebastian.amos8@gmail.com>
2022-05-19 12:48:23 +0100

    initial

--- static/lava.js
+++ static/lava.js
@@ -1,22 +1,28 @@
-const track = document.querySelector('carousel__t');
+const track = document.querySelector('.carousel__t');
 const slides = Array.from(track.children);
-const nextButton = document.querySelector('carousel__b-r');
-const prevButton = document.querySelector('carousel__b-l');
-const dotsNav = document.querySelector('carousel__nav');
+const nextButton = document.querySelector('.carousel__b-r');
+const prevButton = document.querySelector('.carousel__b-l');
+const dotsNav = document.querySelector('.carousel__nav');
 const dots = Array.from(dotsNav.children);
 const slideWidth = slides[0].getBoundingClientRect().width;
-const setSlidePos = (slide,index) => {slide.style.left = slideWidth * index +'px'};
+const setSlidePos = (slide,index) => {slide.style.left = slideWidth * index +'px';}
 
 slides.forEach(setSlidePos);
 
-nextButton.addEventListener("click", event => {
-    const currentSlide = track.querySelector('current-slide');
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translateX (-'+ targetSlide.style.left +')';
+    currentSlide.classList.remove('.current-slide');
+    targetSlide.classList.add('.current-slide');}
+
+prevButton.addEventListener('click', event => {
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+    moveToSlide(track, currentSlide, prevSlide);})
+
+nextButton.addEventListener('click', event => {
+    const currentSlide = track.querySelector('.current-slide');
     const nextSlide = currentSlide.nextElementSibling;
-    const amountToM = nextSlide.style.left;
-    track.style.transform = 'translateX (-'+ amountToM +')';
-    currentSlide.classList.remove('current-slide');
-    nextSlide.classList.add('current-slide');
-})
+    moveToSlide(track, currentSlide, nextSlide);})
 
 function aClick(){
     const accordionItemHs = document.querySelectorAll(".accordion-item-h");
